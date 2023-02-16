2e51a08f8b3df1f339e80d55d0382d1a206118c2
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-16 14:10:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #!/bin/bash
-#include <stdio.h>
-int main () {
-	puts(""Programming is like building a multilingual puzzle");
+puts(""Programming is like building a multilingual puzzle"\n);
 	return 0;
 
