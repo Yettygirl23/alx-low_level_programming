ec2f5bc305ffaf490e838389a8b3f7defb138eab
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-17 05:24:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,3 @@
 #!/bin/bash
-#include <stdio.h>
-/*
-  main - puts ""Programming is like building a multilingual puzzle"\n ;
-	return 0;*/
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 		return (0);
