ebcb702ef522d6f269f33c3f4e4db2a366e8de3b
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 05:17:39 -0800

    Last digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,25 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-       
 /**
- *main - Last digit inputs
+ * main - Last digit chceking
  *
- *Return: Always 0 (sucess)
  */
+
+
 int main(void)
 {
-	int n;
+	int n, ld;
+
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
 	if (ld == 0)
-	{printf("Last digit of %i id %i and is 0\n", n, ld); }
+	{printf("Last digit of %i is %i and is 0\n", n, ld); }
 	else if (ld < 6)
-	{printf("Last digit of %i id %i and is less than 6 and not 0\n", n, ld); }
-	else 
-	{printf("Last digit of %i id %i and is greater than 5\n", n, ld); }
+	{printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld); }
+	else
+	{printf("Last digit of %i is %i and is greater than 5\n", n, ld); }
 	
 	return (0);
 }
