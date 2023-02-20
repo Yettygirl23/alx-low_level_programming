55be6c6f72ee6a9d254e2fd8836a99e48de483a7
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 05:52:34 -0800

    Lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+	char lowercase[];
+	lowercase= 'CAPITAL';
+	putchar("%s in lowercase is represented as %s",
+		lowercase, tolower(lowercase));
+
+	return 0;
+
+
+}
