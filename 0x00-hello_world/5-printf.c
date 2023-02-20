0d3737e1144dbc4e26fa92df4f3a6d667033386e
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 07:50:37 -0800

    5 prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing a new line
+ *
+ * Description: prints out new line
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+                return (0);
+}
