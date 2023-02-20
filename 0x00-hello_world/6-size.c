39161263461dadb16caf10d2a7e219413c44a831
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 08:21:32 -0800

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - sizes
+ *
+ * Description: prints out new line
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)
+	Size of an int: 4 byte(s)
+	Size of a long int: 4 byte(s)
+	Size of a long long int: 8 byte(s)
+	Size of a float: 4 byte(s)");
+	return (0);
+}
