1fddf16a003fb95232a1015aaead1568f2bc0875
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 07:05:39 -0800

    Lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
+/**
+ * main - lowercase
+ *
+ * Description: prints out lowercase alphabets
+ *
+ * Return: Always 0 (Sucess)
+ */
 
 int main()
 {
-	char lowercase [] = "CAPITAL";
-	putchar("%s in lowercase is represented as %s",
-		lowercase, tolower(lowercase));
+	char letter;
+	for (letter = 'a'; letter <= 'z'; lettter++)
+	{putchar(letter); }
+
+	putchar('\n');
 
 	return 0;
 
-
-}
+	}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-Last digit
+lowercase
