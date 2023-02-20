205870e78ff6d79ef0e19514dbdb4020b52704dc
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 14:18:18 -0800

    upper and lower letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints out all alphabets in upper and lower case
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		{putchar(letter); }
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		{putchar(letter); }
+	return (0);
+}
