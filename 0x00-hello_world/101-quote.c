d6570429a3483459448bb28f412c9beb49069ea2
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 15:32:50 -0800

    101 quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing a new line
+ *
+ * Description: prints out new line
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
