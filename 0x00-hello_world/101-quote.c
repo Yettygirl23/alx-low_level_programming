173b8728c6b3a31d5152f30848f3968764a04f2a
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 16:17:43 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - printing a new line
  *
  * Description: prints out new line
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 1 (Sucess)
  */
 
 int main(void)
 {
-	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
