b3e9bc3f369f86ea8aec8096a35ff317c0e93d90
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-20 16:16:48 -0800

    Last digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,6 +23,5 @@ int main(void)
 	{printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld); }
 	else
 	{printf("Last digit of %i is %i and is greater than 5\n", n, ld); }
-	
 	return (0);
 }
