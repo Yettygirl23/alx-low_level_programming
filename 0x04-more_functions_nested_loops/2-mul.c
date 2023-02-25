6c0a1c50461d5e6a1e21806ac9e4331dd7091335
Yettygirl23 <yemisitemitope26@gmail.com>
2023-02-24 16:08:42 -0800

    nested 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies numbers
+ *
+ * @a: number to be multiplied
+ * @b: number to be multiplied
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
